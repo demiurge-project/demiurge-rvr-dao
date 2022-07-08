70235c6e56dbe5fa4067934579af86d685d5ca15
rafftod <ramaluto@gmail.com>
2022-07-08 13:18:05 +0200

    Add center of mass computation

--- src/ReferenceModel1Dot1.cpp
+++ src/ReferenceModel1Dot1.cpp
@@ -107,18 +107,22 @@ void ReferenceModel1Dot1::SetLidarInput(CCI_RVRLidarSensor::TReadings s_lidar_in
     m_sLidarInput = s_lidar_input;
 }
 
-CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors(Real f_alpha_parameter)
+/****************************************/
+/****************************************/
+
+/* Utility function to turn the lidar readings into robot positions */
+std::vector<CCI_RVRLidarSensor::SReading> LidarToRobotPositions(CCI_RVRLidarSensor::TReadings s_lidar_input)
 {
     // identify groups of points which are the robots
     UInt8 n_neigh = -1;
     // array of neighbours id which identifies uniquely a given robot
     // vector of same length as lidar input and with flag -1 to mean "no robot at this angle"
-    std::vector<int> neighbourId(m_sLidarInput.size(), -1);
+    std::vector<int> neighbourId(s_lidar_input.size(), -1);
     // stores the previous index identified as a robot
     UInt16 latestRobotIndex;
-    for (std::size_t i = 0; i != m_sLidarInput.size(); ++i)
+    for (std::size_t i = 0; i != s_lidar_input.size(); ++i)
     {
-        if (m_sLidarInput[i].Value > 0.75 || m_sLidarInput[i].Value < 0.10)
+        if (s_lidar_input[i].Value > 0.75 || s_lidar_input[i].Value < 0.10)
         {
             // we consider it is not a robot beyond 75cm or if the reading is too close to the sensor
             continue;
@@ -138,7 +142,7 @@ CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors
         // if the 2 points have a difference of
         // less than 10 cm in distance to current robot and
         // less than 10 degrees (0.175) in angle then they belong to the same robot
-        if (Abs(m_sLidarInput[i].Value - m_sLidarInput[latestRobotIndex].Value) < 0.1 && Abs(m_sLidarInput[i].Angle - m_sLidarInput[latestRobotIndex].Angle) < CRadians(0.175))
+        if (Abs(s_lidar_input[i].Value - s_lidar_input[latestRobotIndex].Value) < 0.1 && Abs(s_lidar_input[i].Angle - s_lidar_input[latestRobotIndex].Angle) < CRadians(0.175))
         {
             neighbourId[i] = n_neigh;
             latestRobotIndex = i;
@@ -161,11 +165,11 @@ CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors
     {
         // all the positions for this particular group i
         std::vector<CCI_RVRLidarSensor::SReading> groupPositions;
-        for (size_t j = 0; j < m_sLidarInput.size(); j++)
+        for (size_t j = 0; j < s_lidar_input.size(); j++)
         {
             if (neighbourId[j] == i)
             {
-                groupPositions.push_back(m_sLidarInput[j]);
+                groupPositions.push_back(s_lidar_input[j]);
             }
         }
         // compute mean
@@ -177,7 +181,12 @@ CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors
         }
         neighbourPositions[i] = CCI_RVRLidarSensor::SReading(groupSum[0] / groupPositions.size(), CRadians(groupSum[1] / groupPositions.size()));
     }
+    return neighbourPositions;
+}
 
+CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors(Real f_alpha_parameter)
+{
+    auto neighbourPositions = LidarToRobotPositions(m_sLidarInput);
     // we now have the position of each neighbour
     // we can compute the attraction vector
     CVector2 lidarVectorSum(0, CRadians::ZERO);
@@ -195,63 +204,21 @@ CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetAttractionVectorToNeighbors
 /****************************************/
 /****************************************/
 
-// CCI_EPuckRangeAndBearingSensor::SReceivedPacket ReferenceModel1Dot1::GetNeighborsCenterOfMass()
-// {
-//     CCI_EPuckRangeAndBearingSensor::TPackets sRabPackets = m_pcRabMessageBuffer.GetMessages();
-//     CCI_EPuckRangeAndBearingSensor::TPackets::iterator it;
-//     CVector2 sRabVectorSum(0, CRadians::ZERO);
+CCI_RVRLidarSensor::SReading ReferenceModel1Dot1::GetNeighborsCenterOfMass()
+{
+    auto neighbourPositions = LidarToRobotPositions(m_sLidarInput);
+    // we now have the position of each neighbour
+    // we can compute the attraction vector
+    CVector2 lidarVectorSum(0, CRadians::ZERO);
+    for (auto &robotPosition : neighbourPositions)
+    {
+        lidarVectorSum += CVector2(robotPosition.Value, robotPosition.Angle.SignedNormalize());
+    }
+    // divide by the number of robots
+    lidarVectorSum /= neighbourPositions.size();
+    CCI_RVRLidarSensor::SReading cLidarReading;
+    cLidarReading.Value = lidarVectorSum.Length();
+    cLidarReading.Angle = lidarVectorSum.Angle().SignedNormalize();
 
-//     for (it = sRabPackets.begin(); it != sRabPackets.end(); it++)
-//     {
-//         if ((*it)->Data[0] != (UInt32)EpuckDAO::GetRobotIdentifier())
-//         {
-//             sRabVectorSum += CVector2((*it)->Range, (*it)->Bearing.SignedNormalize());
-//         }
-//     }
-
-//     sRabVectorSum /= sRabPackets.size();
-//     CCI_EPuckRangeAndBearingSensor::SReceivedPacket cRaBReading;
-//     cRaBReading.Range = sRabVectorSum.Length();
-//     cRaBReading.Bearing = sRabVectorSum.Angle().SignedNormalize();
-
-//     return cRaBReading;
-// }
-
-// /****************************************/
-// /****************************************/
-
-// std::vector<CCI_EPuckRangeAndBearingSensor::SReceivedPacket *> ReferenceModel1Dot1::GetRangeAndBearingMessages()
-// {
-//     return m_pcRabMessageBuffer.GetMessages();
-// }
-
-// /****************************************/
-// /****************************************/
-
-// void ReferenceModel1Dot1::SetRangeAndBearingMessages(CCI_EPuckRangeAndBearingSensor::TPackets s_packets)
-// {
-//     std::map<UInt32, CCI_EPuckRangeAndBearingSensor::SReceivedPacket *> mapRemainingMessages;
-//     std::map<UInt32, CCI_EPuckRangeAndBearingSensor::SReceivedPacket *>::iterator mapIt;
-//     CCI_EPuckRangeAndBearingSensor::TPackets::iterator it;
-//     m_unNumberNeighbors = 0;
-//     for (it = s_packets.begin(); it < s_packets.end(); ++it)
-//     {
-//         if ((*it)->Data[0] != m_unRobotIdentifier)
-//         {
-//             if (mapRemainingMessages.find((*it)->Data[0]) != mapRemainingMessages.end())
-//             { // If ID not in map, add message.
-//                 mapRemainingMessages[(*it)->Data[0]] = (*it);
-//             }
-//             else if ((*it)->Bearing != CRadians::ZERO)
-//             { // If ID there, overwrite only if the message is valid (correct range and bearing information)
-//                 mapRemainingMessages[(*it)->Data[0]] = (*it);
-//             }
-//         }
-//     }
-//     for (mapIt = mapRemainingMessages.begin(); mapIt != mapRemainingMessages.end(); ++mapIt)
-//     {
-//         m_pcRabMessageBuffer.AddMessage((*mapIt).second);
-//         m_unNumberNeighbors += 1;
-//     }
-//     m_pcRabMessageBuffer.Update();
-// }
+    return cLidarReading;
+}
